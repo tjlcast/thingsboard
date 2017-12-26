5c6a0bc7a26aa5d92c26495ccb5ecce2fd86ca65
cc19931002 <1181018082@qq.com>
2017-12-27 07:52:29 +0800

    service table controller

--- application/src/main/java/org/thingsboard/server/controller/ServiceTableController.java
+++ application/src/main/java/org/thingsboard/server/controller/ServiceTableController.java
@@ -1,5 +1,6 @@
 package org.thingsboard.server.controller;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,9 @@ import org.thingsboard.server.common.data.id.RuleId;
 import org.thingsboard.server.common.data.rule.RuleMetaData;
 import org.thingsboard.server.dao.serviceTable.ServiceTableService;
 import org.thingsboard.server.exception.ThingsboardException;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.thingsboard.server.controller.RuleController.RULE_ID;
 
@@ -34,12 +38,28 @@ public class ServiceTableController extends BaseController{
         String manufacture = service.get("manufacture").getAsString();
         String deviceType = service.get("deviceType").getAsString();
         String model = service.get("model").getAsString();
+        String coordinate = manufacture+"-"+deviceType+"-"+model;
 
-        String des = service.get("description").toString();
-        serviceTable.setCoordinate(manufacture+"-"+deviceType+"-"+model);
-        serviceTable.setDescription(des);
-        serviceTable.setCreatedTime(System.currentTimeMillis());
+        Optional<ServiceTable> serviceTable1=  serviceTableService.findServiceTableByCoordinate(coordinate);
+
+
+        JsonArray array = service.get("description").getAsJsonArray();
+        JsonObject des = new JsonObject();
+        des.add("attributes",new JsonArray());
+        des.add("telemetries",new JsonArray());
+        des.add("services",array);
+
+        serviceTable1.orElse(serviceTable).setCoordinate(coordinate);
+        serviceTable1.orElse(serviceTable).setDescription(des.toString());
+        serviceTable1.orElse(serviceTable).setCreatedTime(System.currentTimeMillis());
        // serviceTable.setId(Ser);
-        return serviceTableService.saveServiceTable(serviceTable);
+        return serviceTableService.saveServiceTable(serviceTable1.orElse(serviceTable));
+    }
+
+    @PreAuthorize("hasAnyAuthority('SYS_ADMIN', 'TENANT_ADMIN')")
+    @RequestMapping(value = "/servicetable/getAll", method = RequestMethod.GET)
+    @ResponseBody
+    public List<ServiceTable> serviceTableList() throws ThingsboardException {
+        return serviceTableService.findServiceTables();
     }
 }
